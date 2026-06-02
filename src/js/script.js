0e3c64bdc884acca49cddd57aa5542f71b82b4cd
Leonardo Scalisse <leonardo.scalisse.silva@gmail.com>
2026-06-02 09:49:45 -0300

    style: Criando estrutura do header html + menu hamburger

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// Menu Hamburger
+document.addEventListener("DOMContentLoaded",()=>{
+    const menuIcone = document.getElementById("menu-icone");
+    const navMenu =document.querySelector("nav");
+    if(menuIcone && navMenu){
+        menuIcone.onclick=()=>{
+                navMenu.classList.toggle("active");
+                menuIcone.classList.toggle("open");
+        }
+    }
+})
