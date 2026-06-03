49baa8a404abf77e1bca50eb4eaab001902cb79f
Leonardo Scalisse <leonardo.scalisse.silva@gmail.com>
2026-06-03 11:17:36 -0300

    style: Estilização da lista de pages e criação delas com DOM

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,48 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
     }
 })
+
+// DOM no main para listar Pages 
+const pages = [
+    {
+        id: 1, // Page 1
+        nome: "Problema Abordado"
+    },
+    {
+        id:2,  // Page 2
+        nome: "Tecnologias Utilizadas"
+    },
+    {
+        id:3,  // Page 3
+        nome: "Objetivos do Projeto"
+    },
+    {
+        id:4,  // Page 4
+        nome: "Público Alvo"
+    },
+    {
+        id:5,  // Page 5
+        nome: "Benefícios do Projeto"
+    },
+    {
+        id:6,  // Page 6
+        nome: "Aplicação do Projeto"
+    }
+]
+
+pages.forEach((page) => {
+    console.log(page.nome);
+})
+pages.forEach((page) => {
+    console.log(`${page.id} - ${page.nome}`);
+});
+
+const listaPages = document.getElementById("lista-pages")
+
+pages.forEach((page) => {
+    listaPages.innerHTML += `
+        <div class="card">
+            <h2>${page.nome}</h2>
+        </div>    
+    `;
+});
